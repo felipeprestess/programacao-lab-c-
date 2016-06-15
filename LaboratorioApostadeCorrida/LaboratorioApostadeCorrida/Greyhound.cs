0bdab5d646f3ad670f7b1d6cec3be9a4226c1216
felipeprestess <felipeadriano_pc@hotmail.com>
2016-06-15 16:22:59 -0300

    Adicionado a classe Greyhound

---
+++ LaboratorioApostadeCorrida/LaboratorioApostadeCorrida/Greyhound.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace LaboratorioApostadeCorrida
+{
+    class Greyhound
+    {
+        public int StartingPosition;//Onde PictureBox inicia
+        public int RacetrackLength;//O tamanho da pista de corrida
+        public PictureBox MyPictureBox = null;//Meu objeto PictureBox
+        public int Location = 0; // Minha posição na pista
+        public Random Randomizer; // Uma instancia de Random
+
+
+        public bool Run()
+        {
+            //Avance 1,2,3 ou 4 espaços aleatoriamente
+            //Atualize a posição de PictureBox no formulário
+            //Retorna true se eu ganhei a corrida
+        }
+
+        public void TakeStartingPosition()
+        {
+            //Redefina minha posição para a linha de partida
+        }
+    }
+}
