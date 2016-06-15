f919bfdc5a8cc5bbf62fa673ea86338092714de5
felipeprestess <felipeadriano_pc@hotmail.com>
2016-06-15 16:05:40 -0300

    Classe Guy inserida com todos atributos e métodos

--- LaboratorioApostadeCorrida/LaboratorioApostadeCorrida/Guy.cs
+++ LaboratorioApostadeCorrida/LaboratorioApostadeCorrida/Guy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace LaboratorioApostadeCorrida
 {
@@ -13,6 +14,29 @@ namespace LaboratorioApostadeCorrida
         public int Cash;//Quanto dinheiro ele tem
 
         //Os últimos dois campos são controles no formulário da GUI dos caras
-       
+        public RadioButton MyRadionButton;//Meu RadioButton
+        public Label MyLabel;// Minha Label
+
+        public void UpdateLabels()
+        {
+            //defina minha etiqueta para a descrição dda minha aposta e a etiqueta em meu
+            //botão de rádio para mostrar meu dinheiro ("João tem 43 reais")
+        }
+
+        public void ClearBet()//Redefina minha aposta para que seja zero
+        {
+
+        }
+
+        public bool PlaceBet(int Amount, int Dog)
+        {
+            //Faça uma nova aposta e armazene-a no meu campo de aposta
+            //Retorne true se o cara teve o dinheiro suficiente para apostar
+        }
+
+        public void Collect(int Winner)
+        {
+            //cobre a minha aposta se eu ganhei
+        }
     }
 }
