8880f409c7cb96c6f9f28a0477dc512e1efa7543
felipeprestess <felipeadriano_pc@hotmail.com>
2016-06-15 15:59:00 -0300

    Incluído a classe Guy

---
+++ LaboratorioApostadeCorrida/LaboratorioApostadeCorrida/Guy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioApostadeCorrida
+{
+    class Guy
+    {
+        public string Name;//O nome do cara
+        public Bet MyBet;//Uma instância de Be() que tem sua aposta
+        public int Cash;//Quanto dinheiro ele tem
+
+        //Os últimos dois campos são controles no formulário da GUI dos caras
+       
+    }
+}
